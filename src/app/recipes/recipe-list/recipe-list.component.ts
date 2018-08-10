28766e62c69f89b3fc052b263ad04d3b4212c25b
Laszlo Dekany <laszlo.dekany@ext.euipo.europa.eu>
2018-08-10 16:38:30 +0200

    Add models to view

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2015/08/10/15/55/bbq-steak-882831_960_720.jpg'),
+    new Recipe('Fish Recipe', 'Delicious fish recipe', 'https://images.pexels.com/photos/36768/food-salmon-teriyaki-cooking.jpg?auto=compress&cs=tinysrgb&h=650&w=940')
+  ];
 
   constructor() { }
 
